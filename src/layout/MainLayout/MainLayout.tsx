2daad7e7de2b56b60ceba891892614f8fe42ce68
Artem Chebotarev <geo.harri40@gmai.com>
2021-12-12 16:49:24 +0300

    Clear project

---
+++ src/layout/MainLayout/MainLayout.tsx
@@ -0,0 +1,14 @@
+import { Box } from '@mui/material'
+import AppBar from '../../components/AppBar/AppBar'
+
+const MainLayout = () => {
+  return (
+    <Box>
+      <AppBar />
+      <Box>
+        df
+      </Box>
+    </Box>
+  )
+}
+export default MainLayout
